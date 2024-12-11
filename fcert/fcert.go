8999b4b066ac1eee3e1d17cfc067cfb04bee8e27
zhangsan3356 <oamsic@163.com>
2024-12-11 18:51:19 +0800

    first commit

--- fcert/fcert.go
+++ fcert/fcert.go
@@ -1,10 +1,62 @@
 package fcert
 
 import (
+	"crypto"
+	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"fmt"
 	"os"
 )
 
+func CalculateSignature(keyPath, filePath string) (string, error) {
+	privateKey, err := loadPrivateKey(keyPath)
+	if err != nil {
+		return "", err
+	}
+	fileHash, err := hashFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf("hash: %x\n", fileHash)
+	signature, err := signHash(fileHash, privateKey)
+	if err != nil {
+		return "", err
+	}
+	sig := base64.StdEncoding.EncodeToString(signature)
+	return sig, nil
+}
+
 func loadPrivateKey(filePath string) (*rsa.PrivateKey, error) {
 	keyData, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	block, _ := pem.Decode(keyData)
+	if block == nil || block.Type != "PRIVATE KEY" {
+		fmt.Println(block.Type)
+		return nil, fmt.Errorf("failed to decode PEM block containing private key")
+	}
+	privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	return privateKey.(*rsa.PrivateKey), err
+}
+
+func hashFile(filePath string) ([]byte, error) {
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	hash := sha256.New()
+	_, err = hash.Write(fileData)
+	if err != nil {
+		return nil, err
+	}
+	return hash.Sum(nil), nil
+}
+
+func signHash(hash []byte, privateKey *rsa.PrivateKey) ([]byte, error) {
+	return rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hash)
 }
--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
-func main() {
-	
-}
+import (
+	"fmt"
 
+	"github.com/ForbiddenR/temp-cert/fcert"
+)
+
+func main() {
+	sig, err := fcert.CalculateSignature("test.key", "README.md")
+	// sig, err := fcert.CalculateSignature("test.key", "README.md")
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("sig", sig, len(sig))
+}
